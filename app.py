ef74164933d46823be861f9898ed60a4fec1657d
Matt Jeanes <matthew.jeanes@myairops.com>
2022-04-03 20:10:59 +0000

    First version of api

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-import falcon, json
+import falcon, os
+from transformers import pipeline
 
-class HelloWorldResource:
-    def on_get(self, req, resp):
-        resp.body = json.dumps({"result": "Hello Word!"})
-        resp.status = falcon.HTTP_OK
-        resp.content_type = falcon.MEDIA_JSON
+current_model = None
+generator = None
+model_folder = os.environ["MODEL_FOLDER"]
+length = int(os.environ["LENGTH"])
+
+class GenerateResource:
+  def on_post(self, req, resp):
+    global current_model
+    global generator
+    obj = req.get_media()
+    model = obj.get("model")
+    text = obj.get("text")
+    model_path = os.path.join(model_folder, model)
+    if not os.path.isdir(model_path):
+      resp.media = {"error": "model does not exist"}
+      resp.status = falcon.HTTP_BAD_REQUEST
+      return
+    if current_model != model:
+      print(f"Loading model {model}")
+      generator = pipeline('text-generation', model=model_path)
+      current_model = model
+    output = generator(text, do_sample=True, min_length=length, max_length=length)
+    output_text = output[0]['generated_text']
+    print(f"Generated for prompt '{text}': {output_text}")
+    resp.media = {"text": output_text}
+    resp.status = falcon.HTTP_OK
 
 app = application = falcon.App()
-app.add_route("/hello_world", HelloWorldResource())
+app.add_route("/api/generate", GenerateResource())
